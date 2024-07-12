b84f753da28b89108325c5b4325006d2f09a9498
Elias Santos Martins <eliasgjt@gmail.com>
2024-07-11 23:11:28 -0300

    cnn scraper class and documentation

--- src/scrapers.py
+++ src/scrapers.py
@@ -7,10 +7,14 @@ from datetime import datetime, timedelta
 
 
 class G1NewsScraper():
+    """Scraper for the G1 news website"""
+
     def __init__(self):
         self.url = 'https://g1.globo.com/'
         
-    def scroll_page(self, driver, n_scrolls):
+    def _scroll_page(self, driver: selenium.webdriver, n_scrolls: int):
+        """Scroll the page by n_scrolls iterations"""
+        
         current_height = driver.execute_script("return document.body.scrollHeight")
 
         for i in range(n_scrolls):
@@ -37,9 +41,12 @@ class G1NewsScraper():
             sleep(4)
             
         
-    def get_scraped_data(self, driver):
+    def _get_scraped_news(self, driver: selenium.webdriver):
+        """Obtain the scraped news from the website"""
         html_source = driver.page_source
         soup = BeautifulSoup(html_source, 'lxml')
+        
+        # the news are split in many containers
         content_blocks = soup.find_all('div', class_ = '_evg')  
         
         titles = []
@@ -51,6 +58,7 @@ class G1NewsScraper():
         for block in content_blocks:
             news_list = block.find_all('div', class_ = 'feed-post-body')
             for news in news_list:
+                # obtain the info of a individual news
                 title = news.find('a', class_ = 'feed-post-link gui-color-primary gui-color-hover')
                 titles.append(title.text)
                 
@@ -66,8 +74,7 @@ class G1NewsScraper():
                 resume = news.find('div', class_='feed-post-body-resumo')
                 resumes.append(None if resume is None else resume.text)
 
-                
-
+        # create the final dataframe
         news_df = pd.DataFrame({
             'Title': titles,
             'Time': times,
@@ -78,7 +85,7 @@ class G1NewsScraper():
         
         return news_df
     
-    def convert_to_datetime(self, time_str):
+    def _convert_to_datetime(self, time_str: str) -> datetime:
         """convert "Há X [time unit]" to datetime"""
         if 'hora' in time_str or 'horas' in time_str:
             hours_ago = int(time_str.split(' ')[1])
@@ -100,19 +107,33 @@ class G1NewsScraper():
             return None  # If the format does not match, return None
         
     
-    def data_cleaning(self, news_df):
-        news_df['Time'] = news_df['Time'].apply(self.convert_to_datetime)
+    def _data_cleaning(self, news_df: pd.DataFrame):
+        """Do simple data cleaning after the scrap"""
+        news_df['Time'] = news_df['Time'].apply(self._convert_to_datetime)
         return news_df
 
-    def scrap_news(self, driver: selenium.webdriver):
+
+    def scrap_news(self, driver: selenium.webdriver) -> pd.DataFrame:
+        """Scrap the news for the G1 website
+
+        Parameters
+        ----------
+        driver : selenium.webdriver
+            Current webdriver
+
+        Returns
+        -------
+        pd.DataFrame
+            The news scraped
+        """
         driver.get(self.url)
         sleep(1)
         
-        self.scroll_page(driver, 10)
+        self._scroll_page(driver, 10)
         
-        news_df = self.get_scraped_data(driver)
+        news_df = self._get_scraped_news(driver)
         
-        news_df = self.data_cleaning(news_df)
+        news_df = self._data_cleaning(news_df)
         
         return news_df
         
@@ -120,31 +141,25 @@ class G1NewsScraper():
         
 class CNNNewsScraper():
     def __init__(self):
-        self.url = 'https://g1.globo.com/'
+        self.url = 'https://www.cnnbrasil.com.br/'
+        
+        # the subpages to acess
+        self.themes = ['politica', 'economia', 'esportes', 'pop']
         
         
-    def scroll_page(self, driver, n_scrolls):
+    def _scroll_page(self, driver, n_scrolls):
+        """Scroll the page by n_scrolls iterations"""
         current_height = driver.execute_script("return document.body.scrollHeight")
+        driver.execute_script(f"window.scrollTo(0,document.body.scrollHeight - 1000)")
 
         for i in range(n_scrolls):
-            # scroll to the end of the page
-            driver.execute_script(f"window.scrollTo(0,document.body.scrollHeight)")
-            sleep(1)
-            
-            # handle the 'Aceitar Cokkies' popup
-            # cookies_button = driver.find_elements(By.CSS_SELECTOR, value='#onetrust-reject-all-handler')
-            # if cookies_button:
-            #     cookies_button[0].click()
-            #     sleep(1)
-            
+            # try to click in the 'Ver mais noticias' button
             try:
                 driver.find_element(By.CSS_SELECTOR, value='.block-list-get-more-btn').click()
             except Exception:
                 print('Error clicking')
             
-            
-            
-            # repeat the scroll
+            # scroll to the end of the page
             driver.execute_script(f"window.scrollTo(0,document.body.scrollHeight - 1000)")
             current_height = driver.execute_script("return document.body.scrollHeight")
                 
@@ -154,17 +169,22 @@ class CNNNewsScraper():
             sleep(4)
             
     
-    def scrape_news_from_page(self, driver, theme):
-        titles = []
-        times = []
-        themes = []
+    def _scrape_news_from_page(self, driver: selenium.webdriver, theme: str) -> pd.DataFrame:
+        """Scrape the news of a given page (url/theme)"""
 
         html_source = driver.page_source
         soup = BeautifulSoup(html_source, 'lxml')
+        
+        # one container contains all the news
         content_block = soup.find('div', class_ = 'col__l--9 col--12')
         news_list = content_block.find_all('li', class_ = 'home__list__item')
         
+        titles = []
+        times = []
+        themes = []
+        
         for news in news_list:
+            # obtain the data for a individual news
             title = news.find('h3', class_ = 'news-item-header__title')
             titles.append(title.text)
 
@@ -180,3 +200,64 @@ class CNNNewsScraper():
         })
 
         return data
+    
+    
+    def _get_scraped_data(self, driver: selenium.webdriver) -> pd.DataFrame:
+        """Scrape news for all the subpages in self.themes"""
+        news_dfs = []
+
+        for theme in self.themes:
+            # scrape the page
+            driver.get(f'{self.url}{theme}')
+            sleep(5)
+            
+            self._scroll_page(driver, 10)
+            
+            data = self._scrape_news_from_page(driver, theme)
+            
+            news_dfs.append(data)
+            
+        # merge all the results of pages
+        news_df = pd.concat(news_dfs, axis='rows')
+        
+        return news_df
+    
+
+    def _convert_to_datetime(self, time_str:str) -> datetime:
+        """Convert date of format 'dd/MM/yyyy às HH:mm' to datetime"""
+        # Remove the 'às' and strip any extra whitespace
+        cleaned_str = time_str.strip().replace(' às ', ' ')
+        # Convert to datetime using the appropriate format
+        return datetime.strptime(cleaned_str, '%d/%m/%Y %H:%M')
+        
+        
+    def _data_cleaning(self, news_df: pd.DataFrame) -> pd.DataFrame:
+        """Do simple data cleaning after the scrap"""
+        news_df['Time'] = news_df['Time'].apply(self._convert_to_datetime)
+
+        news_df['Title'] = news_df['Title'].str.strip()
+        
+        return news_df
+        
+
+    def scrap_news(self, driver: selenium.webdriver) -> pd.DataFrame:
+        """Scrap the news for the CNN website
+
+        Parameters
+        ----------
+        driver : selenium.webdriver
+            Current webdriver
+
+        Returns
+        -------
+        pd.DataFrame
+            The news scraped
+        """
+        driver.get(self.url)
+        
+        news_df = self._get_scraped_data()
+        
+        news_df = self._data_cleaning(news_df)
+        
+        return news_df
+        
